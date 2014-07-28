05b0ff5b27606b0bdaa5660d1cbb61c22dd52d52
Aniruddha Maru <aniruddha@automatic.com>
2014-07-28 21:01:48 +0530

    Adding hmi:interaction scope

---
+++ keys.js
@@ -0,0 +1,9 @@
+/* Replace values below with your Automatic client id and client secret */
+
+module.exports = {
+    automaticClientId: '0e6879dce4c41096c677'
+  , automaticClientSecret: '146444492504b499b0ec634cacd2803eb1170da1'
+  , automaticAuthTokenUrl: 'http://localhost:8000/oauth/access_token/'
+  , automaticAuthorizeUrl: 'http://localhost:8000/oauth/authorize/'
+  , automaticScopes: 'scope:trip:summary scope:vehicle scope:ignition:on scope:ignition:off scope:location scope:mil:on scope:mil:off scope:notification:speeding scope:notification:hard_brake scope:notification:hard_accel scope:region:changed scope:parking:changed scope:notification:stopped scope:hmi:interaction'
+};
